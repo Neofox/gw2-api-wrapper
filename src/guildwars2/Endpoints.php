56f8cbdecacf1870b181043c4bcae159c9ae1e4a
Jérôme Schaeffer <jsc@opcoding.eu>
2016-04-25 09:52:58 +0200

    more endpoints, mode debug addded and query parameters

--- src/guildwars2/Endpoints.php
+++ src/guildwars2/Endpoints.php
@@ -10,21 +10,92 @@ namespace GuildWars2;
 class Endpoints
 {
     // WvW
-    const WVW_MATCHES = '/wvw/matches';
+    const WVW_MATCHES    = '/wvw/matches';
     const WVW_OBJECTIVES = '/wvw/objectives';
+    const WVW_ABILITIES  = '/wvw/abilities';
 
     // Account
-    const ACCOUNT = '/account';
+    const ACCOUNT              = '/account';
     const ACCOUNT_ACHIEVEMENTS = '/account/achievements';
-    const ACCOUNT_BANK = '/account/bank';
-    const ACCOUNT_DYES = '/account/dyes';
-    const ACCOUNT_INVENTORY = '/account/inventory';
-    const ACCOUNT_MATERIALS = '/account/materials';
-    const ACCOUNT_MINIS = '/account/minis';
-    const ACCOUNT_SKINS = '/account/skins';
-    const ACCOUNT_WALLET = '/account/wallet';
+    const ACCOUNT_BANK         = '/account/bank';
+    const ACCOUNT_DYES         = '/account/dyes';
+    const ACCOUNT_INVENTORY    = '/account/inventory';
+    const ACCOUNT_MATERIALS    = '/account/materials';
+    const ACCOUNT_MINIS        = '/account/minis';
+    const ACCOUNT_SKINS        = '/account/skins';
+    const ACCOUNT_WALLET       = '/account/wallet';
+
+    // Achievement
+    const ACHIEVEMENTS                = '/achievements';
+    const ACHIEVEMENTS_DAILY          = '/achievements/daily';
+    const ACHIEVEMENTS_DAILY_TOMORROW = '/achievements/daily/tomorrow';
+    const ACHIEVEMENTS_GROUPS         = '/achievements/groups';
+    const ACHIEVEMENTS_CATEGORIES     = '/achievements/categories';
+
+    // Characters
+    const CHARACTERS                   = '/characters';
+    const CHARACTERS_EQUIPMENT_2       = '/equipment';
+    const CHARACTERS_INVENTORY_2       = '/inventory';
+    const CHARACTERS_RECIPES_2         = '/recipes';
+    const CHARACTERS_SPECIALIZATIONS_2 = '/specializations';
+
+    // Commerce
+    const COMMERCE_EXCHANGE     = '/commerce/exchange';
+    const COMMERCE_LISTINGS     = '/commerce/listings';
+    const COMMERCE_PRICES       = '/commerce/prices';
+    const COMMERCE_TRANSACTIONS = '/commerce/transactions';
+
+    // Event
+    const EVENTS       = '/events';
+    const EVENTS_STATE = '/events-state';
+
+    // Guild
+    const GUILD             = '/guild';
+    const GUILD_LOG_2       = '/log';
+    const GUILD_MEMBERS_2   = '/members';
+    const GUILD_RANKS_2     = '/ranks';
+    const GUILD_STASH_2     = '/stash';
+    const GUILD_TEAMS_2     = '/teams';
+    const GUILD_TREASURY_2  = '/treasury';
+    const GUILD_UPGRADES_2  = '/upgrades';
+    const GUILD_PERMISSIONS = '/guild/permissions';
+    const GUILD_SEARCH      = '/guild/search';
+    const GUILD_UPGRADES    = '/guild/upgrades';
+
+    // PvP
+    const PVP           = '/pvp';
+    const PVP_AMULETS   = '/pvp/amulets';
+    const PVP_GAMES     = '/pvp/games';
+    const PVP_SEASONS   = '/pvp/seasons';
+    const PVP_STANDINGS = '/pvp/standings';
+    const PVP_STATS     = '/pvp/stats';
+
+    // Recipes
+    const RECIPES        = '/recips';
+    const RECIPES_SEARCH = '/recips/search';
 
     // Misc
-    const QUAGGANS= '/quaggans';
-    const WORLDS = '/worlds';
+    const QUAGGANS        = '/quaggans';
+    const WORLDS          = '/worlds';
+    const BUILD           = '/build';
+    const COLORS          = '/colors';
+    const CONTIINENTS     = '/continents';
+    const CURRENCIES      = '/currencies';
+    const EMBLEM          = '/emblem';
+    const FILES           = '/files';
+    const ITEMS           = '/items';
+    const ITEMSTATS       = '/itemstats';
+    const LEADERBOARDS    = '/leaderboards';
+    const LEGENDS         = '/legends';
+    const MAPS            = '/maps';
+    const MATERIALS       = '/materials';
+    const MINIS           = '/minis';
+    const PETS            = '/pets';
+    const PROFESSIONS     = '/professions';
+    const SKILLS          = '/skills';
+    const SKINS           = '/skins';
+    const SPECIALIZATIONS = '/specializations';
+    const TITLES          = '/titles';
+    const TOKENINFO       = '/tokeninfo';
+    const TRAITS          = '/traits';
 }
--- src/guildwars2/Wrapper.php
+++ src/guildwars2/Wrapper.php
@@ -20,30 +20,39 @@ class Wrapper extends AbstractGw2Wrapper
     protected $endpoint;
 
     /**
-     * Wrapper constructor.
-     *
-     * @param string $endpoint
+     * @var string
      */
-    public function __construct($endpoint)
-    {
-        parent::__construct();
-        $this->endpoint = $endpoint;
-    }
-
+    protected $apiKey;
 
     /**
-     * @param null $parameter
+     * @var bool
+     */
+    protected $debug = false;
+
+    /**
+     * @param mixed|null $parameter
+     * @param array      $queryOpt
      *
      * @return mixed
      * @throws \Exception
      * @throws \Gw2Exception
      */
-    public function getResponse($parameter = null)
+    public function getResponse($parameter = null, $queryOpt = [])
     {
-        $result = '';
+        if(!empty($this->getApiKey())){
+            $queryOpt = array_merge($queryOpt, ['access_token' => $this->getApiKey()]);
+        }
+        $options = ['query' => $queryOpt];
 
+        // For not messing with query param when no parameters are sets
+        $parameter = (!empty($parameter) ? '/'.$parameter : null);
         $this->checkParameters($parameter);
-        $response = $this->client->get($this->getVersion() . $this->endpoint . '/' . $parameter);
+
+        // Add some extra informations to the request
+        if($this->getDebug()){
+            $options = array_merge($options, ['debug' => true]);
+        }
+        $response = $this->client->get($this->getVersion() . $this->endpoint . $parameter, $options);
         $result = $response->getBody()->getContents();
 
         if ($response->getStatusCode() != 200) {
@@ -60,10 +69,68 @@ class Wrapper extends AbstractGw2Wrapper
      */
     public function checkParameters($parameter)
     {
+        if(empty($this->getEndpoint())){
+            throw new \Gw2Exception('An endpoint is required ');
+        }
         if (!empty($parameter) && (!is_int($parameter) && !is_string($parameter))) {
             throw new \Gw2Exception(sprintf('the parameter %s must be compatible', $parameter));
         }
-        //TODO: check if parameter is string if its an id or a list of ids
+    }
 
+    /**
+     * @return string
+     */
+    public function getEndpoint() : string
+    {
+        return $this->endpoint;
+    }
+
+    /**
+     * @param string $endpoint
+     *
+     * @return $this
+     */
+    public function setEndpoint(string $endpoint)
+    {
+        $this->endpoint = $endpoint;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiKey()
+    {
+        return $this->apiKey;
+    }
+
+    /**
+     * @param string $apiKey
+     *
+     * @return $this
+     */
+    public function setApiKey(string $apiKey)
+    {
+        $this->apiKey = $apiKey;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getDebug() : bool
+    {
+        return $this->debug;
+    }
+
+    /**
+     * @param bool $debug
+     *
+     * @return $this
+     */
+    public function setDebug(bool $debug)
+    {
+        $this->debug = $debug;
+        return $this;
     }
 }
