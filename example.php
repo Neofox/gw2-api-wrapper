7a945a6bc985f1ddef20b4a133a2f557ac169e95
Jérôme Schaeffer <jsc@opcoding.eu>
2016-04-28 20:59:17 +0200

    add more examples

--- example.php
+++ example.php
@@ -1,21 +1,17 @@
 <?php
 
 
+use GuildWars2\WrappedClass\MiscWrapper;
 use GuildWars2\Wrapper\Endpoints;
 use GuildWars2\Wrapper\Wrapper;
 
 require 'vendor/autoload.php';
 
-const API_KEY  = '7DB220DC-55BF-4C40-A085-065266F89ADACBAF22CF-2FF9-4476-A022-83C4953FCFFF';
-
+// HARD WAY TO CALL THE API (but the most flexible)
 $wrapper = (new Wrapper())->setDebug(false)->setLog(false);
 $wrapper->setEndpoint(Endpoints::QUAGGANS);
+$result = $wrapper->callApi('404');
 
-$result = $wrapper->callApi(getenv('PARAMETER'));
-
-
-$wrappedCall = new \GuildWars2\WrappedClass\MapWrapper(false, true);
-$wrappedResult = $wrappedCall->getWorldInfo(['ids' => '2104,2102,2001'], 'fr');
-
-$test = new \GuildWars2\WrappedClass\MiscWrapper(false, true);
-r($test->getTokenInfo(API_KEY));
+// EASIER WAY TO CALL THE API
+$wrappedCall = new MiscWrapper(false, true);
+$wrappedResult = $wrappedCall->getQuagganInfo(['id' => '404']);
